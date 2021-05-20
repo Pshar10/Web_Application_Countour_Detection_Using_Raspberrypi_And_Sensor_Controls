a1d30fd1593b0f1f26e733cabd0639aa4c250697
Pranav Sharma <pranav.sharma@tu-ilmenau.de>
2021-05-20 21:03:45 +0200

    Added just the code to detect blue color from the camera images

--- practice_room/task1_opencv_control_pranav_adarsh/main.py
+++ practice_room/task1_opencv_control_pranav_adarsh/main.py
@@ -15,7 +15,21 @@ for i in range(5):
 
   
   img = cv2.imdecode(jpg_as_np, cv2.COLOR_BGR2RGB)
-  cv2.imshow('image', img)
+#Converting RGB frame to HSV
+  hsvFrame = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#Adding few lines to detect blue zone
+  blue_lower = np.array([66, 191, 153], np.uint8)
+  blue_upper = np.array([109, 255, 255], np.uint8)
+  blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
+
+  kernal = np.ones((1, 1), "uint8")
+
+  blue_mask = cv2.dilate(blue_mask, kernal)
+  res_blue = cv2.bitwise_and(img, img,
+    mask = blue_mask)
+
+
+  cv2.imshow('image', res_blue) #changing img and putting res_blue
 
   print("Is in zone: ", opencv_controller.is_in_zone())
   print("---------------------------------")
