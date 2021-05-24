f4f29174a9644823ba5a834bb42f883d309c6bc0
Pranav Sharma <pranav.sharma@tu-ilmenau.de>
2021-05-24 22:48:02 +0200

    trying to rotate motors 45 , 75 and 45 ccw for the one cycle

--- practice_room/task2_motor_control_pranav_adarsh/main.py
+++ practice_room/task2_motor_control_pranav_adarsh/main.py
@@ -4,3 +4,4 @@ from motor_controller import MotorController
 motor_controller = MotorController()
 
 motor_controller.start_motor()
+
--- practice_room/task2_motor_control_pranav_adarsh/motor_controller.py
+++ practice_room/task2_motor_control_pranav_adarsh/motor_controller.py
@@ -1,4 +1,5 @@
-from fake_gpio import GPIO # For testing in PC
+from fake_gpio import GPIO
+import time # For testing in PC
 # import RPi.GPIO as GPIO # For testing in Raspberry Pi
 # import ...
 
@@ -11,8 +12,58 @@ class MotorController(object):
     self.PIN_STEP = 25 # do not change
     self.PIN_DIR = 8 # do not change
     self.working = True
+    SPR = 200 # 1 step = 0.225 degree  and we have to rotate for 45 degrees
+
     # ...
     print('Motor started')
 
-  def is_working(self):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(self.PIN_DIR, GPIO.OUT)
+    GPIO.setup(self.PIN_STEP, GPIO.OUT)
+
+  
+    print("Starting to rotate in clockwise direction for 45 degrees")
+    GPIO.output(self.PIN_DIR,GPIO.LOW)
+    step_count = SPR
+    delay = 0.3
+
+    for x in range(step_count):
+      GPIO.output(self.PIN_STEP,GPIO.HIGH)
+      time.sleep(delay)
+      GPIO.output(self.PIN_STEP,GPIO.LOW)
+    print("Motor has rotated clockwise for 45 degrees")
+    time.sleep(0.5)
+
+    print("Aiming for 75 degrees")
+
+    step_count = 133 # for 30 degree more rotation
+
+    for x in range(step_count):
+      GPIO.output(self.PIN_STEP,GPIO.HIGH)
+      time.sleep(delay)
+      GPIO.output(self.PIN_STEP,GPIO.LOW)
+
+    print("Motor has rotated clockwise for 75 degrees now changing the direction ")
+    time.sleep(0.5)
+    
+    GPIO.output(self.PIN_DIR,GPIO.HIGH)
+    step_count = SPR
+    delay = 0.3
+    for x in range(step_count):
+      GPIO.output(self.PIN_STEP,GPIO.HIGH)
+      time.sleep(delay)
+      GPIO.output(self.PIN_STEP,GPIO.LOW)
+    print("Motor has rotated anti-clockwise for 45 degrees")
+    print(".....................................................")
+    print("One cycle complete")
+
+    time.sleep(0.5)
+
+    self.working = False
+
+    print("Motor Working Status: Stopped")
+
+
+  
+  def is_working(self):   #was at last of the code changed the postitpn
     return self.working
