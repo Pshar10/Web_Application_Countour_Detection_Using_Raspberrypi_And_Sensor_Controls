294b86928e88bef4e253d0a2d3b50aa0cb28f586
Pranav Sharma <pranav.sharma@tu-ilmenau.de>
2021-07-31 06:30:34 +0000

    loaded  main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-let monitoring
+let monitoring = false 
 
 /**
  * Function to request the server to start the motor
@@ -6,14 +6,13 @@ let monitoring
 async function requestStartMotor () {
   try {
     // Make request to server
-    await axios.post('/start_motor')
+    await axios.post('/start_motor') //get can also be used
 
     // Update status
-    updateStatus('Working')
+    updateStatus('Processing......') //printing status
     startMonitoring()
   } catch (e) {
-    console.log('Error starting the motor', e)
-    updateStatus('Error starting')
+    updateStatus('The system has stopped, Please start again') // error control
   }
 }
 
@@ -23,9 +22,12 @@ async function requestStartMotor () {
 async function startMonitoring () {
   monitoring = true
   while (monitoring) {
-    let result = await axios.get('/monitor')
-    updateMonitoringData(result.data)
+    let result = await axios.get('/monitor') //result taken from the end point
+    updateMonitoringData(result.data)  //giving result to update function
+    monitoring = false 
+    
   }
+  requestStartMotor ()
 }
 
 /** 
@@ -33,22 +35,64 @@ async function startMonitoring () {
  */
 function stopMonitoring () {
   monitoring = false
+  updateStatus('The system has stopped, Please start again')  // Stopped after updating
+
+  
+  // document.getElementById('inZone').innerHTML= result.inZone
+  //document.getElementById('distance').innerHTML= result.distance
+
+  // if (result.inZone){
+  //   updateStatus('The motor has stopped and the Object is in the Zone')
+  //   document.getElementById('inZone').innerHTML= "Yes　　　　Last　updated　at　"+time
+  // }
+  // else
+  // {
+  //   updateStatus('The motor has stopped and the Object is not in the Zone')
+  //   document.getElementById('inZone').innerHTML= "No　　　　Last　updated　at　"+time
+  // }
+
 }
 
 /**
  * Function to request monitoring data to the server and display it in the main page
  */
-function updateMonitoringData () {
-  // Get HTML elements where results are displayed
-  // ...
+function updateMonitoringData (result){
+  var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(); //updating time of processing
+
+  document.getElementById('Distance').innerHTML= result.distance //getting distance
+
+  if (result.inZone){
+    updateStatus('The motor has stopped after completing the cycle and the Object is in the Zone')
+    document.getElementById('inZone').innerHTML= "Yes　　　　Last　updated　at　"+time 
+  }
+  else
+  {
+    updateStatus('The motor has stopped after completing the cycle and the Object is not in the Zone')
+    document.getElementById('inZone').innerHTML= "No　　　　Last　updated　at　"+time
+  }
+
 }
 
 /**
  * Function to request the server to stop the motor
  */
-function stopSystem () {
+async function stopSystem () {
   //...
-  updateStatus('The motor and sensor monitoring will stop')
+  try {
+    // Make request to server
+    
+    await axios.post('/stop_system') //changed
+
+    updateStatus('The system has stopped, Please start again')
+
+    // Update status
+    stopMonitoring()
+  } catch (e) {
+    
+    updateStatus('The system has stopped, Please start again')
+  }
+
 }
 
 
@@ -56,4 +100,6 @@ function updateStatus(statusText) {
   // Get the HTML element where the status is displayed
   let motor_status_text = document.getElementById('status_text')
   motor_status_text.innerText = statusText
+      
+
 }
